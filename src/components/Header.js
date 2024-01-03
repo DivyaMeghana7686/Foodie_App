4246fb5970d4c8aa3d974016a3becb39e8ed6391
DivyaMeghana7686 <divyamegha25@gmail.com>
2024-01-03 21:00:21 +0530

    new

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import {LOGO_URL} from "../utils/images/logo.jpg";
+import {Link} from "react-router-dom";
+
+const Header=()=>{
+    return(
+        <div className="header border">
+            <div className="logo-container">
+                <img 
+                    className="logo"
+                    src={LOGO_URL}
+                    alt="image"
+                />
+            </div>
+            <div className="nav-items">
+                <p><Link to="/">Home</Link></p>
+                <p><Link to="/about">About Us</Link></p>
+                <p><Link to="/contact">Contact Us</Link></p>
+                <p>Cart</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
